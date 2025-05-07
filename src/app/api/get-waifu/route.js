b0d0de1f45b26413e8550826df33709b1d278bd9
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2025-05-07 20:55:09 +0700

    the waifu pulled before won't show up now unless the user refresh the page

--- src/app/api/get-waifu/route.js
+++ src/app/api/get-waifu/route.js
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 
+var waifuBeforeHolder = null;
+var lockedWaifus = [];
+
 export async function POST(){
   const randomPage = Math.floor(Math.random() * 10) + 1;
   var query = `
@@ -47,22 +50,25 @@ export async function POST(){
       }),
     };
   try{
+    lockedWaifus.push(waifuBeforeHolder);
     const response = await fetch(url, options);
     const data = await response.json();
     const media = data.data.Page.media;
 
     const characters = media.flatMap(media => media.characters.nodes);
     const waifus = characters.filter(char => char.gender === "Female");
-    const randomWaifu = waifus[Math.floor(Math.random() * waifus.length)]
+    const waifuIds = waifus.map(char => char.id);
+    const filterWaifu = waifus.filter(char => !lockedWaifus.includes(char.id));
+    const randomWaifu = filterWaifu[Math.floor(Math.random() * filterWaifu.length)];
 
     const title = randomWaifu.media.nodes.map(item => item.title.english);
     const animeTitle = title[0];
+    waifuBeforeHolder = randomWaifu;
     return NextResponse.json({randomWaifu, animeTitle});
   }
   catch(err){
     return NextResponse.json({
       error : "Error : " + err.message,
-      status: 500,
-    });
+    }, {status : 500});
   }
 }
