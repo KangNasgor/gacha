210d3942e8b0b3cc5c0a9f336a481f5c7beae736
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2025-05-28 19:21:33 +0700

    showing the waifu fetched from my anime list api to inventory page

--- src/app/api/get-inventory/route.js
+++ src/app/api/get-inventory/route.js
@@ -6,12 +6,18 @@ export async function GET(){
     const connection = await connectDB();
     try{
         const user_id = (await cookies()).get('user_id')?.value;
-        const [waifu_ids] = await connection.execute('SELECT * FROM user_waifus WHERE user_id = ? AND type = "ani"', [user_id]);
-        const waifus = waifu_ids.map(item => item.waifu_id);
+        const [waifu_ids_ani] = await connection.execute('SELECT * FROM user_waifus WHERE user_id = ? AND type = "ani"', [user_id]);
+        const [waifu_ids_mal] = await connection.execute('SELECT * FROM user_waifus where user_id = ? AND type = "mal"', [user_id]);
+
+        const waifusAni = waifu_ids_ani.map(item => item.waifu_id);
+        const waifusMal = waifu_ids_mal.map(item => item.waifu_id);
+
+        // Below is a GraphQL query that's using alias to fetch multiple data in one query, the alias is w1,w2, or w3, depends on the index so it's dynamic
+        // Using .join('') to make all the characters joined as one string
         const aniQuery = `
         query {
-        ${waifus.map((id, index) => `
-            w${index}: Character(id : ${id}){
+        ${waifusAni.map((id, index) => `
+            w${index}: Character(id : ${id}){ 
                 id
                 name {
                     full
@@ -29,9 +35,10 @@ export async function GET(){
                     }
                 }
             }
-            `).join('')}
+            `).join('')} 
         }`;
-        const response = await fetch("https://graphql.anilist.co", {
+
+        const responseAni = await fetch("https://graphql.anilist.co", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -43,9 +50,23 @@ export async function GET(){
         })
         .then(res => res.json())
         .then(res => Object.values(res.data));
+
+        const responseMal = await Promise.all(
+            waifusMal.map(id => {
+                return fetch(`https://api.jikan.moe/v4/characters/${id}/full`, {
+                    headers : {
+                        'Content-Type' : 'application/json',
+                        Accept : 'application/json',
+                    }
+                })
+                .then(res => res.json())
+                .then(res => res.data);
+            })
+        );
+        const waifus = [...responseAni, ...responseMal];
         return NextResponse.json({
             success : true,
-            inventory : response
+            inventory : waifus
         });
     }
     catch(err){
--- src/app/inventory/page.js
+++ src/app/inventory/page.js
@@ -9,7 +9,6 @@ export default function Inventory(){
         const fetchInv = async () => {
             const res = await fetch('/api/get-inventory');
             const data = await res.json();
-            console.log(data);
             setInventory(await data.inventory);
         }
 
@@ -21,9 +20,9 @@ export default function Inventory(){
             <div className="grid grid-cols-5 gap-5 font-fun p-4 mx-auto w-11/12 bg-red-300 rounded-md">
             {
                 inventory && inventory.map(waifu => (
-                    <div key={waifu?.id} className="bg-red-400 rounded-md px-2 pt-2 py-4">
-                        <Image src={waifu?.image.medium} height={300} width={200} alt={'Waifu image'} className="mb-4 rounded-md"/>
-                        <h1 className="text-3xl">{waifu?.name.full}</h1>
+                    <div key={waifu.id ?? waifu.mal_id} className="bg-red-400 rounded-md px-2 pt-2 py-4">
+                        <Image src={waifu?.image?.medium ?? waifu?.images?.webp?.image_url} height={300} width={200} alt={'Waifu image'} className="mb-4 rounded-md"/>
+                        <h1 className="text-3xl">{waifu?.name?.full ?? waifu.name}</h1>
                         <h2 className="text-md text-white/70">{waifu?.media?.nodes?.[0]?.title?.english ? waifu?.media?.nodes?.[0]?.title.english : waifu?.anime?.[0].anime.title ?? 'Unknown'}</h2>
                     </div>
                 ))
